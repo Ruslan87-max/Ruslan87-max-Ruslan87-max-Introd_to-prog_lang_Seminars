d2c4b46c18898574d22cb1a1eae789ff12010a23
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-09-27 18:08:45 +0300

    Added lesson2:home_task2

---
+++ Lesson2/Home_task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите координату Х: ");
+int numX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int numY = Convert.ToInt32(Console.ReadLine());
+
+if ((numX == 0 && numY == 0) || numX == 0 || numY == 0) {
+    Console.Write($"Координата точки не находится ни в одной из четвертей");
+}
+else if (numX > 0 && numY > 0) {
+    Console.Write($"Координата точки находится в 1 четверти");
+}
+
+else if (numX < 0 && numY > 0) {
+    Console.Write($"Координата точки находится во 2 четверти");
+}
+
+else if (numX < 0 && numY < 0) {
+    Console.Write($"Координата точки находится в 3 четверти");
+}
+
+else {//if (numX > 0 && numY < 0) {
+    Console.Write($"Координата точки находится в 4 четверти");
+}
+
