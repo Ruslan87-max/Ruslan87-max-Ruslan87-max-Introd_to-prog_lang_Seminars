8fdb706a223fed6b584320cf9f199cc1a6ddd669
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-10-02 20:19:31 +0300

    Added new Lesson4:Home_tadk1

---
+++ Lesson4/Home_task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+
+class Program
+{
+    static int GetSumOfDigitsIsNumber(string str)
+    {
+        int sum = 1;
+        int[] array;
+        bool isNumber = int.TryParse(str, out int result);
+
+        if (isNumber)
+        {
+            array = str.ToCharArray().Select(i => int.Parse(i.ToString())).ToArray();
+            sum = array.Sum();
+        }
+        return sum;
+    }
+    static void Main()
+    {
+        var s = "";
+
+        while (true)
+        {
+            s = Console.ReadLine();
+            char[] matchedItems = Array.FindAll(s.ToCharArray(), x => x == 'q');
+
+            if ((GetSumOfDigitsIsNumber(s) % 2 == 0) || (matchedItems.Length == 1))
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
