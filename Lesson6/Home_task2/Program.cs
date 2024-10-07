6907d0b7b6fd7ea919a1b201fe434f3ed231ff59
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-10-07 20:42:18 +0300

    Added Lesson6:Home_task2_

---
+++ Lesson6/Home_task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
+// “aBcD1ef!-” => “abcd1ef!-”
+
+using System;
+class Program
+{
+    static string GetLowerCaseString(string str)
+    {
+        string str1 = str.ToLower();
+        return str1;
+    }
+
+    static void PrintString(string str)
+    {
+        Console.WriteLine(str);
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string str = Convert.ToString(Console.ReadLine());
+        Console.WriteLine();
+        PrintString(GetLowerCaseString(str));
+    }
+}
+
