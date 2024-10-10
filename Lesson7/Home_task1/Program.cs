af13fa3fa3a28f11f09315baadf98f94c51e15cd
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-10-10 20:35:08 +0300

    Added Lesson7:Home_task_1

---
+++ Lesson7/Home_task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Вывод натуральных чисел в промежутке от M до N 
+// Описание: Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Используйте рекурсию и не используйте циклы.
+
+// Пример:
+// ● Вход: M = 1, N = 5
+// ● Выход: 1, 2, 3, 4, 5
+// ● Вход: M = 4, N = 8
+// ● Выход: 4, 5, 6, 7, 8
+
+using System;
+public class Answer
+{
+    public static void PrintRange(int M, int N)
+    {
+        if (M > N)
+        {
+            Console.WriteLine($"Достигнут верхний предел {N}");
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"{M ++}");
+            PrintRange(M, N);
+        }
+    }
+    static public void Main(string[] args)
+    {
+        int M = 10; // Можно поменять значение для проверки
+        int N = 32; // Можно поменять значение для проверки
+        Console.WriteLine("Числа в промежутке:");
+        PrintRange(M, N);
+    }
+}
